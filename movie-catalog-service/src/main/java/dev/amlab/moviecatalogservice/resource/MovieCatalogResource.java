0f9696e426902e26cad74f5e7bab8d005aeeccf0
Trinh Vu <txvu100@gmail.com>
2022-02-12 16:24:13 -0600

    Switch to use WebClientBuilder

--- movie-catalog-service/src/main/java/dev/amlab/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/dev/amlab/moviecatalogservice/resource/MovieCatalogResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -21,9 +22,13 @@ public class MovieCatalogResource
 	@Autowired
 	private RestTemplate restTemplate;
 
+	@Autowired
+	private WebClient.Builder webClientBuilder;
+
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId)
 	{
+
 
 		List<Rating> ratings = Arrays.asList(
 				new Rating("1234", 4),
@@ -33,7 +38,15 @@ public class MovieCatalogResource
 		System.out.println("> Return a Catalog object");
 
 		return ratings.stream().map(rating -> {
-			Movie movie = restTemplate.getForObject("http://localhost:8200/movies/" + rating.getMovieId(), Movie.class);
+//			Movie movie = restTemplate.getForObject("http://localhost:8200/movies/" + rating.getMovieId(), Movie.class);
+
+			Movie movie = webClientBuilder.build()
+					.get()
+					.uri("http://localhost:8200/movies/" + rating.getMovieId())
+					.retrieve()
+					.bodyToMono(Movie.class)
+					.block();
+
 			return new CatalogItem(movie.getName(), "Description", rating.getRating());
 		}).collect(Collectors.toList());
 
