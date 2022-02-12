0608638a0cd9b50ede761b95106f0f622c40fcfc
Trinh Vu <txvu100@gmail.com>
2022-02-12 12:17:26 -0600

    Add MovieResource.java

--- movie-info-service/src/main/java/dev/amlab/movieinfoservice/resource/MovieResource.java
+++ movie-info-service/src/main/java/dev/amlab/movieinfoservice/resource/MovieResource.java
@@ -1,6 +1,16 @@
 package dev.amlab.movieinfoservice.resource;
 
+import dev.amlab.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
 public class MovieResource
 {
-
+	@RequestMapping("/{movieId}")
+	public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+		return new Movie(movieId, "Test movie name");
+	}
 }
