522b44926af6ecfef00a5d774b5adb0934fc20a4
Trinh Vu <txvu100@gmail.com>
2022-02-12 13:08:49 -0600

    Add Rating class to test communicaton

--- movie-catalog-service/src/main/java/dev/amlab/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/dev/amlab/moviecatalogservice/resource/MovieCatalogResource.java
@@ -1,10 +1,12 @@
 package dev.amlab.moviecatalogservice.resource;
 
 import dev.amlab.moviecatalogservice.models.CatalogItem;
+import dev.amlab.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -14,6 +16,15 @@ public class MovieCatalogResource
 {
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+		List<Rating> ratings = Arrays.asList(
+			new Rating("1234", 4),
+			new Rating("5678", 3)
+		);
+
+		// Get all rated movie
+		// For each movie ID, call movie info service and get details
+		// Put them all together
 		return Collections.singletonList(
 				new CatalogItem("Ironman", "test", 4)
 		);
